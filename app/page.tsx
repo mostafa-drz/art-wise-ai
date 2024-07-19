3b7c75280d5874209b09e46f3cfd665565169011
Mostafa Darehzereshki <mostafa69d@gmail.com>
2024-07-19 20:42:47 +0400

    adds image compression

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,55 @@
 'use client';
 
 import InputForm from './components/InputForm';
-import { Suspense, useState } from 'react';
+import { Suspense, useState, useRef, useEffect } from 'react';
 import Results from './components/Results';
+import imageCompression, { Options } from 'browser-image-compression';
+
+const imageCompressingOptions: Options = {
+  maxSizeMB: 1,
+  maxWidthOrHeight: 800,
+  useWebWorker: true,
+};
 
 export default function Home() {
   const [data, setData] = useState<Output | null>(null);
   const [loading, setLoading] = useState(false);
   const [imageBase64, setImageBase64] = useState<string>('');
+  const worker = useRef<Worker>();
+  const formData = useRef(new FormData());
+
+  useEffect(() => {
+    worker.current = new Worker('/generateBinaryForImageWorker.js');
+
+    worker.current.onmessage = async (e) => {
+      if (typeof e.data !== 'string') throw new Error('Invalid data type');
+      const base64String = e.data;
+      const language = formData.current.get('language') as string;
+      const fileType = (formData.current.get('image') as File).type;
+      setImageBase64(base64String);
+      const res = await fetch('/api/identify', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ image: base64String, language, type: fileType }),
+      });
+      if (!res.ok) throw new Error(await res.text());
+      const responseData = await res.json();
+      setData(responseData); // Set the response data to state
+    };
+  }, []);
+  // Handle messages from the worker
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    if (!worker.current) return;
+
     e.preventDefault();
     setLoading(true); // Start loading
 
     try {
-      const formData = new FormData(e.target as HTMLFormElement);
-      const image = formData.get('image') as File;
-      const language = formData.get('language') as string;
+      formData.current = new FormData(e.target as HTMLFormElement);
+      const image = formData.current.get('image') as File;
 
       if (!image) throw new Error('No image provided');
 
@@ -24,31 +57,15 @@ export default function Home() {
       if (!fileType.startsWith('image/'))
         throw new Error('Invalid file type. Please upload an image.');
 
-      const worker = new Worker('/generateBinaryForImageWorker.js');
-
-      // Handle messages from the worker
-      worker.onmessage = async (e) => {
-        const base64String = e.data;
-        setImageBase64(base64String);
-        const res = await fetch('/api/identify', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ image: base64String, language, type: fileType }),
-        });
-        if (!res.ok) throw new Error(await res.text());
-        const responseData = await res.json();
-        setData(responseData); // Set the response data to state
-      };
+      // compress image on webworker first before sending to server
+      const compressedImage = await imageCompression(image, imageCompressingOptions);
 
       // Send the file to the worker for processing
-      worker.postMessage(image);
+      worker.current.postMessage(compressedImage);
     } catch (e: any) {
       // Handle errors here
       console.error(e);
-    } finally {
-      setLoading(false); // Stop loading
+      setLoading(false);
     }
   }
 

598641b9b57a4de2a055e303408e9a2a5e4b07eb
Mostafa Darehzereshki <mostafa69d@gmail.com>
2025-02-04 12:27:53 -0500

    add comments

--- app/utils/helpers.ts
+++ app/utils/helpers.ts
@@ -11,9 +11,11 @@ export enum GenAiType {
 export const FREE_INCLUDED_CREDITS_FOR_USER = 1000;
 export const GEN_AI_COST = {
   [GenAiType.newSearch]: 1,
-  [GenAiType.generateAudioVersion]: 3,
-  [GenAiType.textConversation]: 2,
-  [GenAiType.liveAudioConversation]: 4,
+  [GenAiType.generateAudioVersion]: 2,
+  // Per text message
+  [GenAiType.textConversation]: 1,
+  // Per Audio message
+  [GenAiType.liveAudioConversation]: 1,
 };
 
 export function handleChargeUser(user: User, transactionType: GenAiType) {
